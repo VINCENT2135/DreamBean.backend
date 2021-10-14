894b8c50f73dc67d5a52d6ade6fb4e013108c7b5
Vincent Ivos <vincentivos1@gmail.com>
2021-10-14 15:10:12 -0400

    User migration/ Gems installed for authentications

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_10_07_163715) do
+ActiveRecord::Schema.define(version: 2021_10_14_190608) do
 
   create_table "bean_varieties", force: :cascade do |t|
     t.string "name"
@@ -29,6 +29,14 @@ ActiveRecord::Schema.define(version: 2021_10_07_163715) do
   create_table "coffee_bean", force: :cascade do |t|
     t.string "title"
     t.string "description"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "username"
+    t.string "email"
+    t.string "password_digest"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
 end
