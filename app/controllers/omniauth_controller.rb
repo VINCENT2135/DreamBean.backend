57994714f8f35cc1003be18e369dcfe0a1b3e78d
Vincent Ivos <vincentivos1@gmail.com>
2021-10-21 19:04:26 -0400

    Routes.rb/Omniauth controller

---
+++ app/controllers/omniauth_controller.rb
@@ -0,0 +1,4 @@
+class OmniAuthController < ApplicationController
+
+  end
+  
--- config/routes.rb
+++ config/routes.rb
@@ -4,3 +4,5 @@ Rails.application.routes.draw do
   resources :coffeebeans
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
+
+get "/auth/facebook/callback", to: "omniauth_callbacks#facebook"
